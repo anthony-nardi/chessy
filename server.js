25adf923d0af5aedb32387b13f27aa1aef6545bd
Anthony Nardi <anthonygt@gmail.com>
2015-09-28 17:45:00 -0400

    update server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.listen(80);
 var io = require('socket.io')(server);
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
